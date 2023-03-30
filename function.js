e222bae1126d4a410c500e98cc6d6a54cd578b50
NissoRuculas <niferu2004@gmail.com>
2023-03-30 11:13:56 -0300

    first commit

---
+++ function.js
@@ -0,0 +1,8 @@
+function toggleList() {
+	var list = document.getElementById("lista");
+	if (list.style.display === "none") {
+		list.style.display = "block";
+	} else {
+		list.style.display = "none";
+	}
+}
